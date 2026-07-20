76fc4ffdb1f03203256b7d274de739da824b26cf
Marsya Aurelia Syahkira <marsya.aureliasyah@gmail.com>
2026-07-20 14:30:11 +0700

    chore: refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(passport.initialize());
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Stall-o API running on port ${PORT}`);
 });
